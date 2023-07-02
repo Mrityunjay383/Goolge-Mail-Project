1b3f8f8fd9c132d0fd2c48ad3b4f7e2e71fb27e6
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2023-07-03 00:35:23 +0530

    Final refactoring

--- helpers/googleAuth/index.js
+++ helpers/googleAuth/index.js
@@ -57,20 +57,4 @@ const authorize = async () => {
   return client;
 };
 
-// async function listLabels(auth) {
-//   const gmail = google.gmail({ version: "v1", auth });
-//   const res = await gmail.users.labels.list({
-//     userId: "me",
-//   });
-//   const labels = res.data.labels;
-//   if (!labels || labels.length === 0) {
-//     console.log("No labels found.");
-//     return;
-//   }
-//   console.log("Labels:");
-//   labels.forEach((label) => {
-//     console.log(`- ${label.name}`);
-//   });
-// }
-
 export default authorize;
